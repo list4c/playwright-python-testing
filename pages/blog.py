73729c663f4f7ec0f50775cf47e2689d37ddf4c4
Bartosz Brozyna <bartosz.brozyna@stxnext.pl>
2023-01-22 18:30:23 +0100

    Added page objects for Blog and Main

---
+++ pages/blog.py
@@ -0,0 +1,22 @@
+from playwright.sync_api import Page, Locator
+
+from pages.base_page import BasePage
+from pages.elements import SearchInput
+from pages.locators import BlogPageLocators
+
+
+class StxBlogPage(BasePage):
+    URL = "/blog"
+    search_input = SearchInput(BlogPageLocators.SEARCH_INPUT)
+
+    def __init__(self, page: Page) -> None:
+        super(StxBlogPage, self).__init__(page)
+        self.search_submit = page.locator(BlogPageLocators.SEARCH_SUBMIT)
+        self.get_searched_posts = page.locator(BlogPageLocators.POSTS)
+
+    def search_articles(self, search_query: str) -> None:
+        self.search_input = search_query
+        self.search_submit.click()
+
+    def get_searched_link(self, link_text: str) -> Locator:
+        return self.page.get_by_role("link", name=link_text)
---
+++ pages/main.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import Page
+
+from pages.base_page import BasePage
+from pages.locators import MainPageLocators
+
+
+class StxMainPage(BasePage):
+    URL = "/"
+
+    def __init__(self, page: Page) -> None:
+        super().__init__(page)
+        self.blog_btn = page.locator(MainPageLocators.HEADER_LINK).filter(has_text="Blog")
+        self.accept_cookies_btn = page.get_by_role(**MainPageLocators.BUTTON_ACCEPT_COOKIES)
+
+    def open_blog(self) -> None:
+        self.blog_btn.click()
+
+    def accept_cookies(self) -> None:
+        self.accept_cookies_btn.click()
