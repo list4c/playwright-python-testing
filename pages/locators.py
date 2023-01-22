e0734f5299a61c897c8ee6e17b86403c24d93744
Bartosz Brozyna <bartosz.brozyna@stxnext.pl>
2023-01-22 18:31:51 +0100

    Moved locators to the separate mnodule

---
+++ pages/locators.py
@@ -0,0 +1,9 @@
+class BlogPageLocators:
+    SEARCH_INPUT = "#newSearchId"
+    SEARCH_SUBMIT = ".blogSearchSubmit a"
+    POSTS = ".postItem"
+
+
+class MainPageLocators:
+    HEADER_LINK = ".HeaderSection li a"
+    BUTTON_ACCEPT_COOKIES = {"role": "button", "name": "Accept"}
