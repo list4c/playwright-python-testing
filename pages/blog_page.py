743c04213cc60755f79e4a38c817b62dad678346
Mariusz Listewnik <mariusz.listewnik@stxnext.pl>
2023-01-31 21:47:06 +0100

    Add changes to blog page: - normalize class names - add base class support - fix imports - add locator handling via separate locators file - add some page objects

--- pages/blog_page.py
+++ pages/blog_page.py
@@ -1,14 +1,15 @@
 from playwright.sync_api import Page
 
+from pages.base_page import BasePage
+from pages.locators import BlogPageLocators
 
-class STXBlogPage:
-    URL = '/blog'
+
+class StxBlogPage(BasePage):
+    URL = "/blog"
 
     def __init__(self, page: Page) -> None:
-        self.page = page
+        super(StxBlogPage, self).__init__(page)
+        self.cookie_consent_button = page.get_by_role("button", name="Accept")
         self.search = page.get_by_role("textbox", name="search")
-        self.main_popup = page.locator(".popupBoxSearchBox")
-
-
-    def load(self) -> None:
-        self.page.goto(self.URL)
+        self.main_popup = page.locator(BlogPageLocators.MAIN_POPUP)
+        self.headings = page.locator(BlogPageLocators.HEADINGS).get_by_role("heading")
