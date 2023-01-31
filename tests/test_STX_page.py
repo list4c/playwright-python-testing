d28a621445f3f2658f71f1665eb547ede5634369
Mariusz Listewnik <mariusz.listewnik@stxnext.pl>
2023-01-31 21:49:31 +0100

    Add changes to tests: - normalize class names - add more page objects - fix imports - add test variables - add new test

--- tests/test_STX_page.py
+++ tests/test_STX_page.py
@@ -1,13 +1,13 @@
 import pytest
 from playwright.sync_api import Page, expect
-from pages.main_page import STXMainPage
-from pages.blog_page import STXBlogPage
+
+from pages.blog_page import StxBlogPage
+from pages.main_page import StxMainPage
 
 
 def test_open_blog_page(
-        page: Page,
-        main_page: STXMainPage,
-        blog_page: STXBlogPage) -> None:
+    page: Page, main_page: StxMainPage, blog_page: StxBlogPage
+) -> None:
 
     # Given the STX main page is displayed
     main_page.load()
@@ -19,25 +19,32 @@ def test_open_blog_page(
     expect(page).to_have_url(blog_page.URL)
 
 
-def test_search_blog_articles(
-        page: Page,
-        blog_page: STXBlogPage) -> None:
+def test_search_blog_articles(page: Page, blog_page: StxBlogPage) -> None:
+
+    keyword = "test"
+    article_title = (
+        "The Business Manager’s Guide to Software Testing and Quality Assurance"
+    )
 
     # Given the STX blog page is displayed
     blog_page.load()
 
     # And the cookie consent button is accepted
 
-    page.get_by_role("button", name="Accept").click()
+    blog_page.cookie_consent_button.click()
 
     # When the user searches for a phrase
     blog_page.search.click()
-    blog_page.search.fill("Software Testing and Quality Assurance")
+    blog_page.search.fill(keyword)
     blog_page.search.press("Enter")
 
     # Then a popup with search results is displayed
     expect(blog_page.main_popup).to_be_visible()
 
-    # And the search results contain the phrase
+    # And the search results have a count of 4
 
-    expect(page.get_by_role("heading", name="The Business Manager’s Guide to Software Testing and Quality Assurance")).to_be_visible()
+    expect(blog_page.headings).to_have_count(4)
+
+    # And the search results contain the article title
+
+    expect(blog_page.headings.filter(has_text=article_title)).to_be_visible()
