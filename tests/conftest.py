c79a46b20b8d35e61bff602b2d7988825032203a
Mariusz Listewnik <mariusz.listewnik@stxnext.pl>
2023-01-31 21:44:54 +0100

    Add changes to conftest: - normalize class names - add viewport settings - fix imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,25 @@
 import pytest
-from pages.main_page import STXMainPage
-from pages.blog_page import STXBlogPage
 from playwright.sync_api import Page
 
-@pytest.fixture
-def blog_page(page: Page) -> STXBlogPage:
-    return STXBlogPage(page)
+from pages.blog_page import StxBlogPage
+from pages.main_page import StxMainPage
+
+
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    return {
+        **browser_context_args,
+        "viewport": {
+            "width": 1920,
+            "height": 1080,
+        }
+    }
 
 @pytest.fixture
-def main_page(page: Page) -> STXMainPage:
-    return STXMainPage(page)
+def blog_page(page: Page) -> StxBlogPage:
+    return StxBlogPage(page)
+
+
+@pytest.fixture
+def main_page(page: Page) -> StxMainPage:
+    return StxMainPage(page)
