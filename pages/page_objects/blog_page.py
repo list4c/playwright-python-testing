deee3da3dbf2b41db5dfc2e0b8afa775f283ba6c
Bartosz Brozyna <bartosz.brozyna@stxnext.pl>
2023-02-15 20:14:45 +0100

    Added popup locator

--- pages/page_objects/blog_page.py
+++ pages/page_objects/blog_page.py
@@ -13,10 +13,11 @@ class BlogPage(BasePage):
         super(BlogPage, self).__init__(page)
         self.search_submit = page.locator(BlogPageLocators.SEARCH_SUBMIT)
         self.get_searched_posts = page.locator(BlogPageLocators.POSTS)
+        self.main_popup = page.locator(BlogPageLocators.MAIN_POPUP)
 
     def search_articles(self, search_query: str) -> None:
         self.search_input = search_query
-        self.search_submit.click(delay=600)
+        self.search_submit.click()
 
     def get_searched_link(self, link_text: str) -> Locator:
         return self.page.get_by_role("link", name=link_text)
