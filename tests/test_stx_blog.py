921b07b46fdca3781ebd604d780cdb60dc845e07
Bartosz Brozyna <bartosz.brozyna@stxnext.pl>
2023-02-15 20:16:25 +0100

    creating independent blog test

--- tests/test_stx_blog.py
+++ tests/test_stx_blog.py
@@ -1,20 +1,18 @@
 from playwright.sync_api import expect
 
 from pages.page_objects.blog_page import BlogPage
-from pages.page_objects.main_page import MainPage
 
 
-def test_blog_page_and_filter_articles(main_page: MainPage, blog_page: BlogPage):
-    expected_link = (
-        "Moderated and Unmoderated Remote Usability Testing: "
-        "What Is It, How to Run It, and What Are the Benefits? "
+def test_blog_page_and_filter_articles(blog_page: BlogPage):
+    article_title = (
+        "The Business Manager’s Guide to Software Testing and Quality Assurance"
     )
     search_query = "test"
 
-    main_page.load()
-    main_page.accept_cookies()
-    main_page.open_blog()
+    blog_page.load()
+    blog_page.accept_cookies()
     blog_page.search_articles(search_query)
 
-    expect(blog_page.get_searched_link(expected_link)).to_be_visible()
+    expect(blog_page.main_popup).to_be_visible()
+    expect(blog_page.get_searched_link(article_title)).to_be_visible()
     expect(blog_page.get_searched_posts).to_have_count(4)
