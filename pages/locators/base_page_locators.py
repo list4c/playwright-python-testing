50a7b4d4e417a79338ac1868e41ec7309e772e80
Mariusz Listewnik <mariusz.listewnik@stxnext.pl>
2023-10-28 20:34:13 +0200

    Refactor base page locators

---
+++ pages/locators/base_page_locators.py
@@ -0,0 +1,2 @@
+class BasePageLocators:
+    COOKIE_ALLOW_ALL = '#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowallSelectionWrapper #CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll'  # # noqa: E501
--- pages/page_objects/base_page.py
+++ pages/page_objects/base_page.py
@@ -1,12 +1,14 @@
 from playwright.sync_api import Page
 
+from pages.locators.base_page_locators import BasePageLocators
+
 
 class BasePage:
     URL: str
 
     def __init__(self, page: Page) -> None:
         self.page = page
-        self.cookie_consent_button = page.get_by_role("button", name="Accept")
+        self.cookie_consent_button = page.locator(BasePageLocators.COOKIE_ALLOW_ALL)
 
     def load(self) -> None:
         self.page.goto(self.URL)
